1ae16286c1fe3ed9988e9c60dd7321c7a75c65f8
Alejandro Gonzalez <alu0101015729@ull.edu.es>
2018-11-07 16:22:31 +0000

    Realizado valor de los elementos por porcion

--- lib/prct06/prct06.rb
+++ lib/prct06/prct06.rb
@@ -1,7 +1,7 @@
 class Etiqueta
-	attr_reader :nombre,:cgrasas,:cgrasassa,:hcarbono,:azucares,:proteinas,:sal,:grasasmono,:grasaspoli,:polialcoholes,:almidon,:fibra,:vitymin
+	attr_reader :nombre,:cgrasas,:cgrasassa,:hcarbono,:azucares,:proteinas,:sal,:grasasmono,:grasaspoli,:polialcoholes,:almidon,:fibra,:vitymin,:porciones,:tamano
 
-	def initialize(nombre,cgrasas,cgrasassa,hcarbono,azucares,proteinas,sal,grasasmono,grasaspoli,polialcoholes,almidon,fibra,vita)
+	def initialize(nombre,cgrasas,cgrasassa,hcarbono,azucares,proteinas,sal,grasasmono,grasaspoli,polialcoholes,almidon,fibra,vita,porciones,tamano)
 		@nombre=nombre
 		@cgrasas=cgrasas
 		@cgrasassa=cgrasassa
@@ -15,6 +15,8 @@ class Etiqueta
 		@almidon=almidon
 		@fibra=fibra
 		@vitymin=vita
+		@porciones=porciones
+		@tamano=tamano
 	end
 
 	def valorenergeticoKJ()
@@ -26,4 +28,54 @@ class Etiqueta
                 veKJ=(cgrasas * 9) + (cgrasassa * 9) + (grasasmono * 9) + (grasaspoli * 9) + (hcarbono * 4) + (polialcoholes * 2.4) + (almidon * 4) + (fibra * 2) + (proteinas * 4) + (sal * 6)
                 veKJ.round(2)
         end
+
+       	def valorgrasasp()
+		vag=(cgrasas * 70) / 100
+		vag.round(2)
+	end
+       	def valorgrasassatup()
+		vag=(cgrasassa * 70) / 100
+                vag.round(2)
+	end	
+        def valorhidratosp()
+		vag=(hcarbono * 70) / 100
+                vag.round(2)
+	end
+        def valorazucaresp()
+		vag=(azucares * 70) / 100
+                vag.round(2)
+	end
+        def valorproteinasp()
+		vag=(proteinas * 70) / 100
+                vag.round(2)
+	end
+        def valorsalp()
+		vag=(sal * 70) / 100
+                vag.round(2)
+	end
+        def valormonograsasp()
+		vag=(grasasmono * 70) / 100
+                vag.round(2)
+	end
+       	def valorgrasaspolip()
+		vag=(grasaspoli * 70) / 100
+                vag.round(2)
+	end
+        def valorpolialcoholesp()
+		vag=(polialcoholes * 70) / 100
+                vag.round(2)
+	end
+        def valoralmidonp()
+		vag=(almidon * 70) / 100
+                vag.round(2)
+	end
+        def valorfibrap()
+		vag=(fibra * 70) / 100
+                vag.round(2)
+	end
+        def valorvityminp()
+		vag=(vitymin * 70) / 100
+                vag.round(2)
+        end
+
 end
--- spec/prct06_spec.rb
+++ spec/prct06_spec.rb
@@ -1,7 +1,7 @@
 RSpec.describe Prct06 do
 
         before :each do
-                @alimento=Etiqueta.new("chocobollo",20.70,2.80,47.30,0.00,9.00,270.00,5.80,11.10,0.00,0.00,2.00,35.10)
+                @alimento=Etiqueta.new("chocobollo",20.70,2.80,47.30,0.00,9.00,270.00,5.80,11.10,0.00,0.00,2.00,35.10,1,75)
         end	
 	describe "# valor de los elementos de la etiqueta" do
 		it "Tipo de alimento" do
@@ -49,6 +49,51 @@ RSpec.describe Prct06 do
 		end
 		it "Valor energetico en kcal" do
                         expect(@alimento.valorenergeticoKcal()).to eq(2212.8)
+                end
+		it "Cantidad de porciones" do
+                        expect(@alimento.porciones).to eq(1)
+                end
+		it "Peso por porcion" do
+                        expect(@alimento.tamano).to eq(75)
+                end
+	end
+	
+	describe "#Valor de los elementos por porcion" do
+		it "Cantidad de grasas por porcion" do
+			expect(@alimento.valorgrasasp()).to eq(14.49)
+		end
+		it "Cantidad de grasas saturadas por porcion" do
+                        expect(@alimento.valorgrasassatup()).to eq(1.96)
+                end
+		it "Cantidad de Hidratos de Carbono por porcion" do
+			expect(@alimento.valorhidratosp()).to eq(33.11)
+		end
+		it "Cantidad de azucares por porcion" do
+			expect(@alimento.valorazucaresp()).to eq(0.0)
+		end
+		it "Cantidad de proteinas por porcion" do
+                        expect(@alimento.valorproteinasp()).to eq(6.3)
+                end
+		it "Cantidad de sal por porcion" do
+                        expect(@alimento.valorsalp()).to eq(189.0)
+                end
+		it "Cantidad de grasas monoinsaturadas por porcion" do
+                        expect(@alimento.valormonograsasp()).to eq(4.06)
+                end
+		it "Cantidad de grasas poliinsaturadas por porcion" do
+                        expect(@alimento.valorgrasaspolip()).to eq(7.77)
+                end
+		it "Cantidad de polialcoholes por porcion" do
+                        expect(@alimento.valorpolialcoholesp()).to eq(0.0)
+                end
+		it "Cantidad de almidon por porcion" do
+                        expect(@alimento.valoralmidonp()).to eq(0.0)
+                end
+		it "Cantidad de Fibra por porcion" do
+                        expect(@alimento.valorfibrap()).to eq(1.4)
+                end
+		it "Cantidad de Vitaminas y Minerales por porcion" do
+                        expect(@alimento.valorvityminp()).to eq(24.57)
                 end
 	end
 end
