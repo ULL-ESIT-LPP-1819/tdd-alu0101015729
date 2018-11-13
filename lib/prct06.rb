67d8cc8b0655fbfddf4281d16479c54cf3965c55
Alejandro Gonzalez <alu0101015729@ull.edu.es>
2018-11-13 16:36:22 +0000

    Creacion de la clase

--- lib/prct06.rb
+++ lib/prct06.rb
@@ -1,6 +1,6 @@
 require "prct06/version"
 require "prct06/prct06"
-
+require "prct06/prct07"
 module Prct06
   class Error < StandardError; end
   # Your code goes here...
---
+++ lib/prct06/prct07.rb
@@ -0,0 +1,10 @@
+Nodo = Struct.new(:value,:next,:prev)
+
+class List
+	attr_reader :head,:tail
+	
+	def initialize
+		@head = nil
+		@tail = nil
+	end
+end
