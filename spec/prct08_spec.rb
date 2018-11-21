aae7ca7fdc0d83a2670d21be7a0b1e7efab6ac9b
Alejandro Gonzalez <alu0101015729@ull.edu.es>
2018-11-21 10:53:35 +0000

    error comprobaciones de imc

--- spec/prct08_spec.rb
+++ spec/prct08_spec.rb
@@ -6,7 +6,13 @@ RSpec.describe Individuo do
 		@persona4=Individuo.new("Sonia Garcia Santos",22,"mujer")
 		@persona5=Individuo.new("Jorge González Cabrera",20,"hombre")
 
-		@paciente1=Paciente.new("Manuel Gonzalez Perez",35,"hombre",82,1.90,80,103)	
+		@paciente1=Paciente.new("Manuel Gonzalez Perez",35,"hombre",82,1.90)
+		@paciente2=Paciente.new("Kevin Garcia Peña",20,"hombre",90,1.70)
+		@paciente3=Paciente.new("Miriam Rodriguez Mendez",23,"mujer",40,1.60)
+		@paciente4=Paciente.new("Sonia Garcia Santos",22,"mujer",70,1.6)
+		@paciente5=Paciente.new("Jorge González Cabrera",20,"hombre",70,1.87)
+
+		@lista=List.new()	
 	end
 
 	describe "hacer to_s de la clase Individuo" do
@@ -22,6 +28,27 @@ RSpec.describe Individuo do
 
 		it "metodo IMC" do
 			expect(@paciente1.imc).to eq("Aceptable")
+		end
+		it "Comparaciones IMC" do
+			aceptable = []
+			sobrepeso = []
+			obesidad = []
+			@lista.insert_tail(@paciente1)
+			@lista.insert_tail(@paciente2)
+			@lista.insert_tail(@paciente3)
+			@lista.insert_tail(@paciente4)
+			@lista.insert_tail(@paciente5)
+			until @lista.vacia do
+				valor=@lista.extraer_head
+				if(valor.imc=="Aceptable")
+					aceptable.push(valor.imc1)
+				elsif(valor.imc=="Sobrepeso")
+					sobrepeso.push(valor.imc1)
+				else
+					obesidad.push(valor.imc1)
+				end
+			end
+			expect(aceptable).to eq([])
 		end
 	end
 end
