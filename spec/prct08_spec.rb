2b44e54d71c0441cc6ce0a48e0ef77b4ef457183
Alejandro Gonzalez <alu0101015729@ull.edu.es>
2018-11-21 10:05:26 +0000

    Error to_s

--- spec/prct08_spec.rb
+++ spec/prct08_spec.rb
@@ -9,9 +9,15 @@ RSpec.describe Individuo do
 		@paciente1=Paciente.new("Manuel Gonzalez Perez",35,"hombre",82,1.90,80,103)	
 	end
 
-	describe "hacer to_s" do
+	describe "hacer to_s de la clase Individuo" do
 		it "definir metodo to_s" do
 			expect(@persona1.to_s).to eq("Nombre: Manuel Gonzalez Perez Años: 35 Sexo: hombre")
+		end
+	end
+
+	describe "hacer to_s de la clase Paciente" do
+		it "definir metodo to_s" do
+			expect(@paciente1.to_s).to eq("Nombre: Manuel Gonzalez Perez Años: 35 Sexo: hombre Peso: 82 Altura: 1.90 Circunferencia Cintura: 80 Circunferencia Cadera: 103" )
 		end
 	end
 end
