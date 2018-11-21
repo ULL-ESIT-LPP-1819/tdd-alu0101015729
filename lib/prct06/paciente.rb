3b5bdffe1f95d0a6940c63965cbe3ffd19e318f5
Alejandro Gonzalez <alu0101015729@ull.edu.es>
2018-11-21 10:16:47 +0000

    realizado metodo imc de la clase paciente

--- lib/prct06/paciente.rb
+++ lib/prct06/paciente.rb
@@ -12,4 +12,17 @@ class Paciente < Individuo
 	def to_s
 		"#{super.to_s} Peso: #{@peso} Altura: #{@altura} Circunferencia Cintura: #{@circunferenciaca} Circunferencia Cadera: #{@circunferenciaci}"
 	end
+
+	def imc
+		valor = @peso / ( @altura * @altura )
+		if valor < 5
+			return "Delgado"
+		elsif valor >=18.5 and valor < 25
+			return "Aceptable"
+		elsif valor >=25 and valor < 30
+			return "Sobrepeso"
+		else
+			return "Obesidad"
+		end
+	end
 end
