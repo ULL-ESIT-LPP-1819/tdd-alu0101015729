0e9c640036e1e17f6dc48124960f3f9ff15d783e
Alejandro Gonzalez <alu0101015729@ull.edu.es>
2018-12-12 10:06:32 +0000

    Cuerpo de las pruebas

--- lib/prct06/paciente.rb
+++ lib/prct06/paciente.rb
@@ -1,11 +1,12 @@
 class Paciente < Individuo
-	attr_reader :peso,:altura
+	attr_reader :peso,:altura,:factor
 	include Enumerable
 	#inicializa las variables de instancia de la clase
-	def initialize(nombre,edad,sexo,weight,tall)
+	def initialize(nombre,edad,sexo,weight,tall,factor)
 		super(nombre,edad,sexo)
 		@peso=weight
 		@altura=tall
+		@factor=factor
 	end
 	#funcion que retorna un string
 	def to_s
@@ -29,4 +30,29 @@ class Paciente < Individuo
 		valor = @peso / ( @altura * @altura )
 		return valor
 	end
+
+	def peso_teorico_ideal
+		peso_teorico_ideal=(@altura-150)*0.75+50
+	end
+
+	def gasto_energetico_basal
+		if(@sexo="hombre")
+			gasto_energetico_basal=(10*@peso)+(6.25*@altura)-(5*@agnos)+5
+		else
+			gasto_energetico_basal=(10*@peso)+(6.25*@altura)-(5*@agnos)-161
+		end
+	end
+
+	def efecto_termogeno
+		efecto_termogeno= gasto_energetico_basal*0.10
+	end
+
+	def gasto_actividad_fisica
+		gasto_actividad_fisica=gasto_energetico_basal * factor
+	end
+
+	def gasto_energetico_total
+		gasto_energetico_total=gasto_energetico_basal+efecto_termogeno+gasto_actividad_fisica
+	end
+
 end
