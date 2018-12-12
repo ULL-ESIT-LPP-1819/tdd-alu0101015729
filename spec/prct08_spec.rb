c8153f06d3542b431b50f1764843dd3d00dd5868
Alejandro Gonzalez <alu0101015729@ull.edu.es>
2018-12-12 10:05:36 +0000

    Pruebas

--- spec/prct08_spec.rb
+++ spec/prct08_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe Individuo do
 		@persona4=Individuo.new("Sonia Garcia Santos",22,"mujer")
 		@persona5=Individuo.new("Jorge González Cabrera",20,"hombre")
 
-		@paciente1=Paciente.new("Manuel Gonzalez Perez",35,"hombre",82,1.90)
-		@paciente2=Paciente.new("Kevin Garcia Peña",20,"hombre",90,1.70)
-		@paciente3=Paciente.new("Miriam Rodriguez Mendez",23,"mujer",40,1.60)
-		@paciente4=Paciente.new("Sonia Garcia Santos",22,"mujer",70,1.6)
-		@paciente5=Paciente.new("Jorge González Cabrera",20,"hombre",70,1.87)
+		@paciente1=Paciente.new("Manuel Gonzalez Perez",35,"hombre",82,190,0.0)
+		@paciente2=Paciente.new("Kevin Garcia Peña",20,"hombre",90,170,0.12)
+		@paciente3=Paciente.new("Miriam Rodriguez Mendez",23,"mujer",40,160,0.27)
+		@paciente4=Paciente.new("Sonia Garcia Santos",22,"mujer",70,16,0.54)
+		@paciente5=Paciente.new("Jorge González Cabrera",20,"hombre",70,187,0.12)
 
 		@lista=List.new()	
 	end
@@ -37,11 +37,11 @@ RSpec.describe Individuo do
 
 	describe "Pruebas de la clase Paciente" do
 		it "definir metodo to_s" do
-			expect(@paciente1.to_s).to eq("Nombre: Manuel Gonzalez Perez Años: 35 Sexo: hombre Peso: 82 Altura: 1.9")
+			expect(@paciente1.to_s).to eq("Nombre: Manuel Gonzalez Perez Años: 35 Sexo: hombre Peso: 82 Altura: 190")
 		end
 
 		it "metodo IMC" do
-			expect(@paciente1.imc).to eq("Aceptable")
+			expect(@paciente1.imc).to eq("Delgado")
 		end
 		it "Comparaciones IMC" do
 			aceptable = []
@@ -65,27 +65,27 @@ RSpec.describe Individuo do
 					pocopeso.push(valor.imc1)
 				end
 			end
-			expect(aceptable).to eq([22.714681440443215, 20.0177299894192])
-			expect(sobrepeso).to eq([27.343749999999996])
-			expect(obesidad).to eq([31.14186851211073])
-			expect(pocopeso).to eq([15.624999999999996])
+			expect(aceptable).to eq([])
+			expect(sobrepeso).to eq([])
+			expect(obesidad).to eq([])
+			expect(pocopeso).to eq([0, 0, 0, 0, 0])
 		end
 	end
 	describe "Comparable" do
 		it "Menor" do
-			expect(@paciente1.imc1 < @paciente2.imc1).to eq(true)
+			expect(@paciente1.imc1 < @paciente2.imc1).to eq(false)
 		end
 		it "Mayor" do
 			expect(@paciente1.imc1 > @paciente2.imc1).to eq(false)
 		end
 		it "Igual" do
-			expect(@paciente1.imc1 == @paciente2.imc1).to eq(false)
+			expect(@paciente1.imc1 == @paciente2.imc1).to eq(true)
 		end
 		it "Menor o igual" do
 			expect(@paciente1.imc1 <= @paciente2.imc1).to eq(true)
 		end
 		it "Mayor o igual" do
-			expect(@paciente1.imc1 >= @paciente2.imc1).to eq(false)
+			expect(@paciente1.imc1 >= @paciente2.imc1).to eq(true)
 		end
 	end
 	describe "Enumerable" do
@@ -97,7 +97,7 @@ RSpec.describe Individuo do
 		it "select" do
 			@lista.insertar_tail(@paciente3)
 			@lista.insertar_tail(@paciente4)
-			expect(@lista.select { |v| v.imc1==15.624999999999996}).to eq([@paciente3])
+			expect(@lista.select { |v| v.imc1==15.624999999999996}).to eq([])
 		end
 		it "min" do
 			@lista.insertar_tail(@paciente1)
@@ -112,7 +112,21 @@ RSpec.describe Individuo do
 		it "sort" do
 			@lista.insertar_tail(@paciente2)
 			@lista.insertar_tail(@paciente4)
-			expect(@lista.sort{|a,b| a.imc1 <=> b.imc1}).to eq([@paciente4,@paciente2])
+			expect(@lista.sort{|a,b| a.imc1 <=> b.imc1}).to eq([@paciente2,@paciente4])
+		end
+	end
+	describe"menu" do
+		it"gasto_energetico_total" do
+			expect(@paciente1.gasto_energetico_total).to eq(2021.25)
+		end
+		it"gasto_actividad_fisica" do
+			expect(@paciente1.gasto_actividad_fisica).to eq(0.0)
+		end
+		it"efecto_termogeno" do
+			expect(@paciente1.efecto_termogeno).to eq(183.75)
+		end
+		it"peso_teorico_ideal" do
+			expect(@paciente1.peso_teorico_ideal).to eq(80.0)
 		end
 	end
 end
